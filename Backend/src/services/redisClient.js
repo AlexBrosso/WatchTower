462f24551bcca9516b04c60c353d5a984b58c6a6
Alex Brosso <alexbrosso7@gmail.com>
2025-10-14 01:33:20 -0300

    refactor(redis): restructure connection flow and add health check system

--- Backend/src/services/redisClient.js
+++ Backend/src/services/redisClient.js
@@ -3,12 +3,17 @@ require("dotenv").config();
 
 let redis;
 let redisConnected = false;
+let redisConnectionProcessStarted = false;
 
 async function createRedisClient() {
+  if(!redisConnectionProcessStarted){
+    redisConnectionProcessStarted = true;
     redis = createClient({
         url: process.env.REDIS_URL || "redis://localhost:6379",
         socket: {
-            reconnectStrategy: 30000
+            reconnectStrategy: (retries) => {
+              Math.min(retries * 2500, 30000);
+            }
         }
     });
 
@@ -29,22 +34,45 @@ async function createRedisClient() {
           }
         });
 
-    try{
-        await redis.connect();
-    } catch (err){
-        console.error("Failed to connect to Redis. Cache will be temporarily disabled - " + err.message);
-        redis = null;
+    try {
+      await redis.connect();
+    } catch (err) {
+      console.error("Failed to connect to Redis. Cache will be temporarily disabled - " + err.message);
+      redis = null;
+    } finally {
+      redisConnectionProcessStarted = false; // sempre libera o processo
     }
 
     return redis;
+  }
 }
 
 async function getRedisClient() {
   if (!redisConnected) {
-    console.warn('Redis client not connected. Returning null client.');
+    if(!await isRedisAlive(redis)){
+      createRedisClient();
+      console.warn('Redis client is not initiliazed yet. Tryng start the service and for now returning null client.');
+    }
+    else console.warn('Redis client not connected. Returning null client.');
     return null;
   }
   return redis;
+}
+
+async function isRedisAlive(client, timeoutMs = 1500) {
+  if (!client) return false;
+
+  const pingPromise = client.ping();
+  const timeoutPromise = new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("Redis ping timeout")), timeoutMs)
+  );
+
+  try {
+    const res = await Promise.race([pingPromise, timeoutPromise]);
+    return res === "PONG";
+  } catch {
+    return false;
+  }
 }
 
 module.exports = { createRedisClient, getRedisClient };
